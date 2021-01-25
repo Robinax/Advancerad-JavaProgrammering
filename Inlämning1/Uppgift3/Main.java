8b4c962587dfb2c26e55e33b92f7cb71f6aca64d
Robinax <69907783+Robinax@users.noreply.github.com>
2021-01-25 10:35:56 +0100

    Add files via upload

--- Inlämning1/Uppgift3/Main.java
+++ Inlämning1/Uppgift3/Main.java
@@ -10,7 +10,7 @@ public class Main {
         String[] names = {"James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", "Matthew", "Anthony", "Donald", "Mark", "Paul", "Steven", "Andrew", "Kenneth", "Joshua", "Kevin", "Brian", "George", "Edward", "Ronald", "Timothy", "Jason", "Jeffrey", "Ryan", "Jacob", "Gary", "Nicholas", "Eric", "Jonathan", "Stephen", "Larry", "Justin", "Scott", "Brandon", "Benjamin", "Samuel", "Frank", "Gregory", "Raymond", "Alexander", "Patrick", "Jack", "Dennis", "Jerry",};
         List<MyObject> objects = new ArrayList<>(); //skapar listan
         List<MyObject> objects1 = new ArrayList<>(); //skapar listan
-        MyObject ga = new MyObject(false, 4, "namn2");
+
         for(int i = 0; i<50; i++){
             int val=(int)(Math.random()*50)+1;
             int rName=(int)(Math.random()*49)+1;
@@ -24,14 +24,13 @@ public class Main {
 
             MyObject gs = new MyObject(bool, val, names[rName]); //gör en instance av objektet med värdena
             objects.add(gs); //lägger in objektet i listan
-            objects1.add(gs);
+
         }
-        objects1.add(ga);
-        objects.add(ga);
+
 
         System.out.println("1:"+Arrays.toString(objects
                 .stream()
-                .filter(x-> x.getValue()>20)
+                .filter(s-> s.getValue()>20)
 
                 .toArray()));
 
@@ -42,6 +41,15 @@ public class Main {
                 .getValue())
                 .average()
                 .getAsDouble());
+
+
+
+        Arrays.toString((objects.stream().filter(x-> x.isBool()).map(x -> x.name = "this is true").toArray()));
+
+        System.out.println("3:"+ objects);
+
+        System.out.println("eller");
+
 
         System.out.println("3:"+Arrays.toString(objects
                 .stream()
